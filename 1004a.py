e2317951c8f17d81491d45cc5d27f1af080068a3
DanniXu1997 <40332963@live.napier.ac.uk>
2017-10-05 10:51:32 +0000

    1005b.py

--- 1004a.py
+++ 1004a.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/Hello/<name>')
+@app.route('/hello/<name>')
 def hello(name=None):
   user = {'name':name}
   return render_template('hello.html', user=user)
